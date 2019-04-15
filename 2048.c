5a9bcccf56d18443d1fbfe7e96ee902c4161048c
JooHyeng Lee <dlwngud1230@naver.com>
2019-04-15 01:48:04 -0700

    ready key moving

--- 2048.c
+++ 2048.c
@@ -1,100 +1,99 @@
-/*
- ============================================================================
- Name        : 2048.c
- Author      : Maurits van der Schee
- Description : Console version of the game "2048" for GNU/Linux
- ============================================================================
- */
-//bombbomb;;
+#define _CRT_SECURE_NO_WARNINGS
 #define _XOPEN_SOURCE 500
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
-#include <termios.h>
+//#include <unistd.h>
+//#include<io.h>
+//#include <termios.h>
 #include <stdbool.h>
 #include <stdint.h>
 #include <time.h>
 #include <signal.h>
+#include <conio.h>
 
 #define SIZE 4
-uint32_t score=0;
-uint8_t scheme=0;
+unsigned long score = 0;
+unsigned char scheme = 0;
 
-void getColor(uint8_t value, char *color, size_t length) {
-	uint8_t original[] = {8,255,1,255,2,255,3,255,4,255,5,255,6,255,7,255,9,0,10,0,11,0,12,0,13,0,14,0,255,0,255,0};
-	uint8_t blackwhite[] = {232,255,234,255,236,255,238,255,240,255,242,255,244,255,246,0,248,0,249,0,250,0,251,0,252,0,253,0,254,0,255,0};
-	uint8_t bluered[] = {235,255,63,255,57,255,93,255,129,255,165,255,201,255,200,255,199,255,198,255,197,255,196,255,196,255,196,255,196,255,196,255};
-	uint8_t *schemes[] = {original,blackwhite,bluered};
-	uint8_t *background = schemes[scheme]+0;
-	uint8_t *foreground = schemes[scheme]+1;
+void getColor(unsigned char value, char *color, unsigned int length) {
+	unsigned char original[] = { 8,255,1,255,2,255,3,255,4,255,5,255,6,255,7,255,9,0,10,0,11,0,12,0,13,0,14,0,255,0,255,0 };
+	//unsigned char blackwhite[] = { 232,255,234,255,236,255,238,255,240,255,242,255,244,255,246,0,248,0,249,0,250,0,251,0,252,0,253,0,254,0,255,0 };
+	//unsigned char bluered[] = { 235,255,63,255,57,255,93,255,129,255,165,255,201,255,200,255,199,255,198,255,197,255,196,255,196,255,196,255,196,255,196,255 };
+	//unsigned char *schemes[] = { original,blackwhite,bluered };
+	unsigned char *schemes[] = { original };
+	//unsigned char *schemes[] = { original};
+	unsigned char *background = schemes[scheme] + 0;
+	unsigned char *foreground = schemes[scheme] + 1;
 	if (value > 0) while (value--) {
-		if (background+2<schemes[scheme]+sizeof(original)) {
-			background+=2;
-			foreground+=2;
+		if (background + 2 < schemes[scheme] + sizeof(original)) {
+			background += 2;
+			foreground += 2;
 		}
 	}
-	snprintf(color,length,"\033[38;5;%d;48;5;%dm",*foreground,*background);
+	snprintf(color, length, "\033[38;5;%d;48;5;%dm", *foreground, *background);
 }
 
-void drawBoard(uint8_t board[SIZE][SIZE]) {
-	uint8_t x,y;
+void drawBoard(unsigned char board[SIZE][SIZE]) { // uint8_t = unsigned char 
+	unsigned char x, y;
 	char color[40], reset[] = "\033[m";
 	printf("\033[H");
 
-	printf("2048.c %17d pts\n\n",score);
+	printf("2048.c %17lu pts\n\n", score);   // %17d -> %17lu (unsigned long형식이니깐)
 
-	for (y=0;y<SIZE;y++) {
-		for (x=0;x<SIZE;x++) {
-			getColor(board[x][y],color,40);
-			printf("%s",color);
+	for (y = 0; y < SIZE; y++) {
+		for (x = 0; x < SIZE; x++) {
+			getColor(board[x][y], color, 40);
+			printf("%s", color);
 			printf("       ");
-			printf("%s",reset);
+			printf("%s", reset);
 		}
 		printf("\n");
-		for (x=0;x<SIZE;x++) {
-			getColor(board[x][y],color,40);
-			printf("%s",color);
-			if (board[x][y]!=0) {
+		for (x = 0; x < SIZE; x++) {
+			getColor(board[x][y], color, 40);
+			printf("%s", color);
+			if (board[x][y] != 0) {
 				char s[8];
-				snprintf(s,8,"%u",(uint32_t)1<<board[x][y]);
-				uint8_t t = 7-strlen(s);
-				printf("%*s%s%*s",t-t/2,"",s,t/2,"");
-			} else {
-				printf("   ·   ");
+				snprintf(s, 8, "%lu", (unsigned long)1 << board[x][y]);   //%u -> %lu (unsigned long형식이니깐 ) 
+				unsigned char t = (unsigned char)(7 - strlen(s));  //unsigned char로 강제변환 
+				printf("%*s%s%*s", t - t / 2, "", s, t / 2, "");
 			}
-			printf("%s",reset);
+			else {
+				printf("       ");
+			}
+			printf("%s", reset);
 		}
 		printf("\n");
-		for (x=0;x<SIZE;x++) {
-			getColor(board[x][y],color,40);
-			printf("%s",color);
+		for (x = 0; x < SIZE; x++) {
+			getColor(board[x][y], color, 40);
+			printf("%s", color);
 			printf("       ");
-			printf("%s",reset);
+			printf("%s", reset);
 		}
 		printf("\n");
 	}
 	printf("\n");
-	printf("        ←,↑,→,↓ or q        \n");
+	printf("        a,w,d,s or q        \n");
 	printf("\033[A"); // one line up
 }
 
-uint8_t findTarget(uint8_t array[SIZE],uint8_t x,uint8_t stop) {
-	uint8_t t;
+unsigned char findTarget(unsigned char array[SIZE], unsigned char x, unsigned char stop) {
+	unsigned char t;
 	// if the position is already on the first, don't evaluate
-	if (x==0) {
+	if (x == 0) {
 		return x;
 	}
-	for(t=x-1;;t--) {
-		if (array[t]!=0) {
-			if (array[t]!=array[x]) {
+	for (t = x - 1;; t--) {
+		if (array[t] != 0) {
+			if (array[t] != array[x]) {
 				// merge is not possible, take next position
-				return t+1;
+				return t + 1;
 			}
 			return t;
-		} else {
+		}
+		else {
 			// we should not slide further, return this one
-			if (t==stop) {
+			if (t == stop) {
 				return t;
 			}
 		}
@@ -103,27 +102,28 @@ uint8_t findTarget(uint8_t array[SIZE],uint8_t x,uint8_t stop) {
 	return x;
 }
 
-bool slideArray(uint8_t array[SIZE]) {
+bool slideArray(unsigned char array[SIZE]) {
 	bool success = false;
-	uint8_t x,t,stop=0;
+	unsigned char x, t, stop = 0;
 
-	for (x=0;x<SIZE;x++) {
-		if (array[x]!=0) {
-			t = findTarget(array,x,stop);
+	for (x = 0; x < SIZE; x++) {
+		if (array[x] != 0) {
+			t = findTarget(array, x, stop);
 			// if target is not original position, then move or merge
-			if (t!=x) {
+			if (t != x) {
 				// if target is zero, this is a move
-				if (array[t]==0) {
-					array[t]=array[x];
-				} else if (array[t]==array[x]) {
+				if (array[t] == 0) {
+					array[t] = array[x];
+				}
+				else if (array[t] == array[x]) {
 					// merge (increase power of two)
 					array[t]++;
 					// increase score
-					score+=(uint32_t)1<<array[t];
+					score += (unsigned long)1 << array[t];
 					// set stop to avoid double merge
-					stop = t+1;
+					stop = t + 1;
 				}
-				array[x]=0;
+				array[x] = 0;
 				success = true;
 			}
 		}
@@ -131,30 +131,30 @@ bool slideArray(uint8_t array[SIZE]) {
 	return success;
 }
 
-void rotateBoard(uint8_t board[SIZE][SIZE]) {
-	uint8_t i,j,n=SIZE;
-	uint8_t tmp;
-	for (i=0; i<n/2; i++) {
-		for (j=i; j<n-i-1; j++) {
+void rotateBoard(unsigned char board[SIZE][SIZE]) {
+	unsigned char i, j, n = SIZE;
+	unsigned char tmp;
+	for (i = 0; i < n / 2; i++) {
+		for (j = i; j < n - i - 1; j++) {
 			tmp = board[i][j];
-			board[i][j] = board[j][n-i-1];
-			board[j][n-i-1] = board[n-i-1][n-j-1];
-			board[n-i-1][n-j-1] = board[n-j-1][i];
-			board[n-j-1][i] = tmp;
+			board[i][j] = board[j][n - i - 1];
+			board[j][n - i - 1] = board[n - i - 1][n - j - 1];
+			board[n - i - 1][n - j - 1] = board[n - j - 1][i];
+			board[n - j - 1][i] = tmp;
 		}
 	}
 }
 
-bool moveUp(uint8_t board[SIZE][SIZE]) {
+bool moveUp(unsigned char board[SIZE][SIZE]) {
 	bool success = false;
-	uint8_t x;
-	for (x=0;x<SIZE;x++) {
+	unsigned char x;
+	for (x = 0; x < SIZE; x++) {
 		success |= slideArray(board[x]);
 	}
 	return success;
 }
 
-bool moveLeft(uint8_t board[SIZE][SIZE]) {
+bool moveLeft(unsigned char board[SIZE][SIZE]) {
 	bool success;
 	rotateBoard(board);
 	success = moveUp(board);
@@ -164,7 +164,7 @@ bool moveLeft(uint8_t board[SIZE][SIZE]) {
 	return success;
 }
 
-bool moveDown(uint8_t board[SIZE][SIZE]) {
+bool moveDown(unsigned char board[SIZE][SIZE]) {
 	bool success;
 	rotateBoard(board);
 	rotateBoard(board);
@@ -174,7 +174,7 @@ bool moveDown(uint8_t board[SIZE][SIZE]) {
 	return success;
 }
 
-bool moveRight(uint8_t board[SIZE][SIZE]) {
+bool moveRight(unsigned char board[SIZE][SIZE]) {
 	bool success;
 	rotateBoard(board);
 	rotateBoard(board);
@@ -184,23 +184,23 @@ bool moveRight(uint8_t board[SIZE][SIZE]) {
 	return success;
 }
 
-bool findPairDown(uint8_t board[SIZE][SIZE]) {
+bool findPairDown(unsigned char board[SIZE][SIZE]) {
 	bool success = false;
-	uint8_t x,y;
-	for (x=0;x<SIZE;x++) {
-		for (y=0;y<SIZE-1;y++) {
-			if (board[x][y]==board[x][y+1]) return true;
+	unsigned char x, y;
+	for (x = 0; x < SIZE; x++) {
+		for (y = 0; y < SIZE - 1; y++) {
+			if (board[x][y] == board[x][y + 1]) return true;
 		}
 	}
 	return success;
 }
 
-uint8_t countEmpty(uint8_t board[SIZE][SIZE]) {
-	uint8_t x,y;
-	uint8_t count=0;
-	for (x=0;x<SIZE;x++) {
-		for (y=0;y<SIZE;y++) {
-			if (board[x][y]==0) {
+unsigned char countEmpty(unsigned char board[SIZE][SIZE]) {
+	unsigned char x, y;
+	unsigned char count = 0;
+	for (x = 0; x < SIZE; x++) {
+		for (y = 0; y < SIZE; y++) {
+			if (board[x][y] == 0) {
 				count++;
 			}
 		}
@@ -208,9 +208,9 @@ uint8_t countEmpty(uint8_t board[SIZE][SIZE]) {
 	return count;
 }
 
-bool gameEnded(uint8_t board[SIZE][SIZE]) {
+bool gameEnded(unsigned char board[SIZE][SIZE]) {
 	bool ended = true;
-	if (countEmpty(board)>0) return false;
+	if (countEmpty(board) > 0) return false;
 	if (findPairDown(board)) return false;
 	rotateBoard(board);
 	if (findPairDown(board)) ended = false;
@@ -220,41 +220,41 @@ bool gameEnded(uint8_t board[SIZE][SIZE]) {
 	return ended;
 }
 
-void addRandom(uint8_t board[SIZE][SIZE]) {
+void addRandom(unsigned char board[SIZE][SIZE]) {
 	static bool initialized = false;
-	uint8_t x,y;
-	uint8_t r,len=0;
-	uint8_t n,list[SIZE*SIZE][2];
+	unsigned char x, y;
+	unsigned char r, len = 0;
+	unsigned char n, list[SIZE*SIZE][2];
 
 	if (!initialized) {
-		srand(time(NULL));
+		srand((unsigned int)time(NULL));
 		initialized = true;
 	}
 
-	for (x=0;x<SIZE;x++) {
-		for (y=0;y<SIZE;y++) {
-			if (board[x][y]==0) {
-				list[len][0]=x;
-				list[len][1]=y;
+	for (x = 0; x < SIZE; x++) {
+		for (y = 0; y < SIZE; y++) {
+			if (board[x][y] == 0) {
+				list[len][0] = x;
+				list[len][1] = y;
 				len++;
 			}
 		}
 	}
 
-	if (len>0) {
-		r = rand()%len;
+	if (len > 0) {
+		r = rand() % len;
 		x = list[r][0];
 		y = list[r][1];
-		n = (rand()%10)/9+1;
-		board[x][y]=n;
+		n = (rand() % 10) / 9 + 1;
+		board[x][y] = n;
 	}
 }
 
-void initBoard(uint8_t board[SIZE][SIZE]) {
-	uint8_t x,y;
-	for (x=0;x<SIZE;x++) {
-		for (y=0;y<SIZE;y++) {
-			board[x][y]=0;
+void initBoard(unsigned char board[SIZE][SIZE]) {
+	unsigned char x, y;
+	for (x = 0; x < SIZE; x++) {
+		for (y = 0; y < SIZE; y++) {
+			board[x][y] = 0;
 		}
 	}
 	addRandom(board);
@@ -263,116 +263,144 @@ void initBoard(uint8_t board[SIZE][SIZE]) {
 	score = 0;
 }
 
-void setBufferedInput(bool enable) {
-	static bool enabled = true;
-	static struct termios old;
-	struct termios new;
-
-	if (enable && !enabled) {
-		// restore the former settings
-		tcsetattr(STDIN_FILENO,TCSANOW,&old);
-		// set the new state
-		enabled = true;
-	} else if (!enable && enabled) {
-		// get the terminal settings for standard input
-		tcgetattr(STDIN_FILENO,&new);
-		// we want to keep the old setting to restore them at the end
-		old = new;
-		// disable canonical mode (buffered i/o) and local echo
-		new.c_lflag &=(~ICANON & ~ECHO);
-		// set the new settings immediately
-		tcsetattr(STDIN_FILENO,TCSANOW,&new);
-		// set the new state
-		enabled = false;
-	}
-}
+//void setBufferedInput(bool enable) {
+//   static bool enabled = true;
+//   static struct termios old;
+//   struct termios new;
+//
+//   if (enable && !enabled) {
+//      // restore the former settings
+//      tcsetattr(STDIN_FILENO, TCSANOW, &old);
+//      // set the new state
+//      enabled = true;
+//   }
+//   else if (!enable && enabled) {
+//      // get the terminal settings for standard input
+//      tcgetattr(STDIN_FILENO, &new);
+//      // we want to keep the old setting to restore them at the end
+//      old = new;
+//      // disable canonical mode (buffered i/o) and local echo
+//      new.c_lflag &= (~ICANON & ~ECHO);
+//      // set the new settings immediately
+//      tcsetattr(STDIN_FILENO, TCSANOW, &new);
+//      // set the new state
+//      enabled = false;
+//   }
+//}
 
 /*
 int test() {
-	uint8_t array[SIZE];
-	// these are exponents with base 2 (1=2 2=4 3=8)
-	uint8_t data[] = {
-		0,0,0,1,	1,0,0,0,
-		0,0,1,1,	2,0,0,0,
-		0,1,0,1,	2,0,0,0,
-		1,0,0,1,	2,0,0,0,
-		1,0,1,0,	2,0,0,0,
-		1,1,1,0,	2,1,0,0,
-		1,0,1,1,	2,1,0,0,
-		1,1,0,1,	2,1,0,0,
-		1,1,1,1,	2,2,0,0,
-		2,2,1,1,	3,2,0,0,
-		1,1,2,2,	2,3,0,0,
-		3,0,1,1,	3,2,0,0,
-		2,0,1,1,	2,2,0,0
-	};
-	uint8_t *in,*out;
-	uint8_t t,tests;
-	uint8_t i;
-	bool success = true;
+unsigned char array[SIZE];
+// these are exponents with base 2 (1=2 2=4 3=8)
+unsigned char data[] = {
+0,0,0,1,   1,0,0,0,
+0,0,1,1,   2,0,0,0,
+0,1,0,1,   2,0,0,0,
+1,0,0,1,   2,0,0,0,
+1,0,1,0,   2,0,0,0,
+1,1,1,0,   2,1,0,0,
+1,0,1,1,   2,1,0,0,
+1,1,0,1,   2,1,0,0,
+1,1,1,1,   2,2,0,0,
+2,2,1,1,   3,2,0,0,
+1,1,2,2,   2,3,0,0,
+3,0,1,1,   3,2,0,0,
+2,0,1,1,   2,2,0,0
+};
+unsigned char *in,*out;
+unsigned char t,tests;
+unsigned char i;
+bool success = true;
 
-	tests = (sizeof(data)/sizeof(data[0]))/(2*SIZE);
-	for (t=0;t<tests;t++) {
-		in = data+t*2*SIZE;
-		out = in + SIZE;
-		for (i=0;i<SIZE;i++) {
-			array[i] = in[i];
-		}
-		slideArray(array);
-		for (i=0;i<SIZE;i++) {
-			if (array[i] != out[i]) {
-				success = false;
-			}
-		}
-		if (success==false) {
-			for (i=0;i<SIZE;i++) {
-				printf("%d ",in[i]);
-			}
-			printf("=> ");
-			for (i=0;i<SIZE;i++) {
-				printf("%d ",array[i]);
-			}
-			printf("expected ");
-			for (i=0;i<SIZE;i++) {
-				printf("%d ",in[i]);
-			}
-			printf("=> ");
-			for (i=0;i<SIZE;i++) {
-				printf("%d ",out[i]);
-			}
-			printf("\n");
-			break;
-		}
-	}
-	if (success) {
-		printf("All %u tests executed successfully\n",tests);
-	}
-	return !success;
+tests = (sizeof(data)/sizeof(data[0]))/(2*SIZE);
+for (t=0;t<tests;t++) {
+in = data+t*2*SIZE;
+out = in + SIZE;
+for (i=0;i<SIZE;i++) {
+array[i] = in[i];
+}
+slideArray(array);
+for (i=0;i<SIZE;i++) {
+if (array[i] != out[i]) {
+success = false;
+}
+}
+if (success==false) {
+for (i=0;i<SIZE;i++) {
+printf("%d ",in[i]);
+}
+printf("=> ");
+for (i=0;i<SIZE;i++) {
+printf("%d ",array[i]);
+}
+printf("expected ");
+for (i=0;i<SIZE;i++) {
+printf("%d ",in[i]);
+}
+printf("=> ");
+for (i=0;i<SIZE;i++) {
+printf("%d ",out[i]);
+}
+printf("\n");
+break;
+}
+}
+if (success) {
+printf("All %u tests executed successfully\n",tests);
+}
+return !success;
 }
 */
 
 
 void signal_callback_handler(int signum) {
 	printf("         TERMINATED         \n");
-	setBufferedInput(true);
+	//setBufferedInput(true);
 	printf("\033[?25h\033[m");
 	exit(signum);
 }
-
+void action(bool success,unsigned char board[SIZE][SIZE]) {
+	if (success) {
+		drawBoard(board);
+		//usleep(150000);
+		addRandom(board);
+		drawBoard(board);
+		if (gameEnded(board)) {
+			printf("         GAME OVER          \n");
+			
+		}
+	}
+	/*if (c == 'q') {
+		printf("        QUIT? (y/n)         \n");
+		scanf("%c", &c);
+		if (c == 'y') {
+	
+		}
+		drawBoard(board);
+	}
+	if (c == 'r') {
+		printf("       RESTART? (y/n)       \n");
+		scanf("%c", &c);
+		if (c == 'y') {
+			initBoard(board);
+		}
+		drawBoard(board);
+	}*/
+}
 int main(int argc, char *argv[]) {
-	uint8_t board[SIZE][SIZE];
-	char c;
+	unsigned char board[SIZE][SIZE];
+	int c;
 	bool success;
 
 	/*
 	if (argc == 2 && strcmp(argv[1],"test")==0) {
-		return test();
+	return test();
 	}
 	*/
-	if (argc == 2 && strcmp(argv[1],"blackwhite")==0) {
+	if (argc == 2 && strcmp(argv[1], "blackwhite") == 0) {
 		scheme = 1;
 	}
-	if (argc == 2 && strcmp(argv[1],"bluered")==0) {
+	if (argc == 2 && strcmp(argv[1], "bluered") == 0) {
 		scheme = 2;
 	}
 
@@ -382,60 +410,71 @@ int main(int argc, char *argv[]) {
 	signal(SIGINT, signal_callback_handler);
 
 	initBoard(board);
-	setBufferedInput(false);
-	while (true) {
-		c=getchar();
-		if (c == -1){ //TODO: maybe replace this -1 with a pre-defined constant(if it's in one of header files)
-		    	puts("\nError! Cannot read keyboard input!");
-			break;
-		}
-		switch(c) {
-			case 97:	// 'a' key
-			case 104:	// 'h' key
-			case 68:	// left arrow
-				success = moveLeft(board);  break;
-			case 100:	// 'd' key
-			case 108:	// 'l' key
-			case 67:	// right arrow
-				success = moveRight(board); break;
-			case 119:	// 'w' key
-			case 107:	// 'k' key
-			case 65:	// up arrow
-				success = moveUp(board);    break;
-			case 115:	// 's' key
-			case 106:	// 'j' key
-			case 66:	// down arrow
-				success = moveDown(board);  break;
-			default: success = false;
-		}
-		if (success) {
-			drawBoard(board);
-			usleep(150000);
-			addRandom(board);
-			drawBoard(board);
-			if (gameEnded(board)) {
-				printf("         GAME OVER          \n");
-				break;
+	//setBufferedInput(false);
+	while (1) {
+		if (_kbhit()) {               // 2. while문안에서 키보드 눌렸을 시 if문이 실행된다.
+			c = _getch();           // 3. 방향키가 입력됬을 때 224 00 이 버퍼에 있다. 224부터 빼준다. 
+			if (c == 224) {           // 4. -32로 입력되면
+				c = _getch();        // 5. 뒤의 값 00을 판별하여 좌우상하 출력
+				switch (c) {
+				case 75:
+					printf("좌로 이동\n");
+					success = moveLeft(board);  
+					action(success, board);
+					break;
+
+				case 77:
+					printf("우로 이동\n");
+					success = moveRight(board); 
+					action(success, board);
+					break;
+
+				case 72:
+					printf("위로 이동\n");
+					success = moveUp(board);   
+					action(success, board);
+					break;
+
+				case 80:
+					printf("아래로 이동\n");
+					success = moveDown(board);  
+					action(success, board);
+					break;
+				}
 			}
 		}
-		if (c=='q') {
-			printf("        QUIT? (y/n)         \n");
-			c=getchar();
-			if (c=='y') {
-				break;
-			}
-			drawBoard(board);
-		}
-		if (c=='r') {
-			printf("       RESTART? (y/n)       \n");
-			c=getchar();
-			if (c=='y') {
-				initBoard(board);
-			}
-			drawBoard(board);
-		}
+	
+
+	//while (true) {
+	//	scanf("%c", &c);    //c = getchar() -> scanf("%c", &c)로 수정
+	//	getchar(); //추가 시켜줘야 게임을 끝낼수 있다.
+	//	if (c == -1) { //TODO: maybe replace this -1 with a pre-defined constant(if it's in one of header files)
+	//		puts("\nError! Cannot read keyboard input!");
+	//		break;
+	//	}
+	//	switch (c) {
+	//	case 97:   // 'a' key
+	//	case 104:   // 'h' key
+	//	case 68:   // left arrow
+	//		success = moveLeft(board);  break;
+	//	case 100:   // 'd' key
+	//	case 108:   // 'l' key
+	//	case 67:   // right arrow
+	//		success = moveRight(board); break;
+	//	case 119:   // 'w' key
+	//	case 107:   // 'k' key
+	//	case 65:   // up arrow
+	//		success = moveUp(board);    break;
+	//	case 115:   // 's' key
+	//	case 106:   // 'j' key
+	//	case 66:   // down arrow
+	//		success = moveDown(board);  break;
+	//	default: success = false;
+	//	}
+
+
 	}
-	setBufferedInput(true);
+	//setBufferedInput(true);
 
 	printf("\033[?25h\033[m");
 
